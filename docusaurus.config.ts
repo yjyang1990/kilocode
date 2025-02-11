3b8f36b48d0b84c44e4a6d7e2c53106341525763
Matt Rubens <mrubens@users.noreply.github.com>
2025-02-11 17:08:15 -0500

    Serve docs from the root (#22)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,6 +37,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          routeBasePath: '/',
         },
         theme: {
           customCss: './src/css/custom.css',
