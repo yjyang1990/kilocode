99678b0eaa266c15bdbb02a6c7bc55bac667f303
anx <hakt0r@users.noreply.github.com>
2025-03-26 10:11:50 +0100

    DevX - kiloToken switches to development server (#92)
    * typo in URL to reach out to us
    
    * extract env from token -> use localhost if development

--- src/api/providers/kilocode.ts
+++ src/api/providers/kilocode.ts
@@ -10,14 +10,27 @@ import { SingleCompletionHandler, getModelParams } from "../index"
 export class KiloCodeHandler extends BaseProvider implements SingleCompletionHandler {
 	private options: ApiHandlerOptions
 	private client: Anthropic
+	private baseURL: string = "https://kilocode.ai"
 
 	constructor(options: ApiHandlerOptions) {
 		super()
 		this.options = options
+		this.getBaseURL()
 		this.client = new Anthropic({
 			authToken: this.options.kilocodeToken,
-			baseURL: "https://kilocode.ai/api/claude/",
+			baseURL: `${this.baseURL}/api/claude/`,
 		})
+	}
+
+	private getBaseURL() {
+		try {
+			const token = this.options.kilocodeToken as string
+			const payload_string = token.split(".")[1]
+			const payload = JSON.parse(Buffer.from(payload_string, "base64").toString())
+			if (payload.env === "development") this.baseURL = "http://localhost:3000"
+		} catch (_error) {
+			console.warn("Failed to get base URL from Kilo Code token")
+		}
 	}
 
 	async *createMessage(systemPrompt: string, messages: Anthropic.Messages.MessageParam[]): ApiStream {
@@ -172,7 +185,7 @@ export class KiloCodeHandler extends BaseProvider implements SingleCompletionHan
 					type: "text",
 					text:
 						`ERROR: ${error.message || "Failed to communicate with Kilo Code API"}\n\n` +
-						"If you need any help please check http://kilocode.ai to reach out to us",
+						"If you need any help please check https://kilocode.ai to reach out to us",
 				}
 			}
 
