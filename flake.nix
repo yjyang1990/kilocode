467412bc07973e5152b6580359aa7b4ace47c30c
Catriel Müller <catrielmuller@gmail.com>
2025-06-23 10:49:31 -0300

    refactor: upgrade nixos repository to v25.05 and setup the envrc for load automatically

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kilo Code development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }: let
