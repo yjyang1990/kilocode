cb22f80aad84165369df399162bcbd19c4fac2e1
Catriel Müller <catrielmuller@gmail.com>
2025-06-26 11:11:12 -0300

    fix: improve performance on models with long responses

--- src/core/task/Task.ts
+++ src/core/task/Task.ts
@@ -1409,7 +1409,13 @@ export class Task extends EventEmitter<ClineEvents> {
 							}
 
 							// Present content to user.
-							presentAssistantMessage(this)
+							// kilocode_change start
+							try {
+								await presentAssistantMessage(this)
+							} catch (error) {
+								console.error(`Error presenting assistant message: ${error.message}`)
+							}
+							// kilocode_change end
 							break
 						}
 					}
@@ -1503,7 +1509,13 @@ export class Task extends EventEmitter<ClineEvents> {
 				// `pWaitFor` before making the next request. All this is really
 				// doing is presenting the last partial message that we just set
 				// to complete.
-				presentAssistantMessage(this)
+				// kilocode_change strart
+				try {
+					await presentAssistantMessage(this)
+				} catch (error) {
+					console.error(`Error presenting assistant message: ${error.message}`)
+				}
+				// kilocode_change end
 			}
 
 			updateApiReqMsg()
