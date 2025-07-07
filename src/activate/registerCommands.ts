e448830359ba359e514bba47689222335a281835
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2025-07-07 14:25:25 +0200

    Use light and dark icons where appropriate

--- src/activate/registerCommands.ts
+++ src/activate/registerCommands.ts
@@ -285,8 +285,8 @@ export const openClineInNewTab = async ({ context, outputChannel }: Omit<Registe
 	// TODO: Use better svg icon with light and dark variants (see
 	// https://stackoverflow.com/questions/58365687/vscode-extension-iconpath).
 	newPanel.iconPath = {
-		light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "kilo.png"),
-		dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "kilo.png"),
+		light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "kilo-light.svg"),
+		dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "kilo-dark.svg"),
 	}
 
 	await tabProvider.resolveWebviewView(newPanel)
