c2baf8f5657608b3f5c9b94f83714afd8f20b866
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2025-07-29 17:46:47 +0200

    Change the definition of excessive recursion

--- src/core/kilocode.ts
+++ src/core/kilocode.ts
@@ -2,7 +2,7 @@ import { TelemetryService } from "@roo-code/telemetry"
 import { TelemetryEventName } from "@roo-code/types"
 
 export function reportExcessiveRecursion(functionName: string, recursionDepth: number) {
-	if (recursionDepth > 1 && Number.isInteger(Math.log10(recursionDepth))) {
+	if (recursionDepth > 10 && Number.isInteger(Math.log10(recursionDepth))) {
 		const memory = { ...process.memoryUsage() }
 		const props = { functionName, recursionDepth, memory }
 		TelemetryService.instance.captureEvent(TelemetryEventName.EXCESSIVE_RECURSION, props)
