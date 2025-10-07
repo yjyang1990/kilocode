09c7dc4d3f6aa9d31ea4eb0ad28067f4d0cd0a19
Matt Cowger <matt.cowger@sigmacomputing.com>
2025-10-07 15:36:18 -0700

    Add JSON-based tool definitions

---
+++ src/core/prompts/tools/native-tools/index.ts
@@ -0,0 +1,41 @@
+import askFollowupQuestion from "./ask_followup_question.json"
+import attemptCompletion from "./attempt_completion.json"
+import browserAction from "./browser_action.json"
+import codebaseSearch from "./codebase_search.json"
+import editFile from "./edit_file.json"
+import executeCommand from "./execute_command.json"
+import fetchInstructions from "./fetch_instructions.json"
+import generateImage from "./generate_image.json"
+import insertContent from "./insert_content.json"
+import listCodeDefinitionNames from "./list_code_definition_names.json"
+import listFiles from "./list_files.json"
+import newTask from "./new_task.json"
+import readFile from "./read_file.json"
+import runSlashCommand from "./run_slash_command.json"
+import searchAndReplace from "./search_and_replace.json"
+import searchFiles from "./search_files.json"
+import switchMode from "./switch_mode.json"
+import updateTodoList from "./update_todo_list.json"
+import writeToFile from "./write_to_file.json"
+
+export const nativeTools = {
+	askFollowupQuestion,
+	attemptCompletion,
+	browserAction,
+	codebaseSearch,
+	editFile,
+	executeCommand,
+	fetchInstructions,
+	generateImage,
+	insertContent,
+	listCodeDefinitionNames,
+	listFiles,
+	newTask,
+	readFile,
+	runSlashCommand,
+	searchAndReplace,
+	searchFiles,
+	switchMode,
+	updateTodoList,
+	writeToFile,
+}
