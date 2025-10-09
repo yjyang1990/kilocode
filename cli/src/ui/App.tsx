aff11e036a24287fb1a2d90e90a5c7dcd70155f1
Remon Oldenbeuving <remon@kilocode.ai>
2025-10-09 14:39:23 +0200

    Remove unused code

--- cli/src/ui/App.tsx
+++ cli/src/ui/App.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Provider as JotaiProvider } from "jotai"
-import { logs } from "../services/logs.js"
-import { Loader } from "./Loader.js"
+import { UI } from "./UI.js"
 
 type JotaiStore = any
 
@@ -20,25 +19,9 @@ export interface AppProps {
 }
 
 export const App: React.FC<AppProps> = ({ store, options, onExit }) => {
-	const [mounted, setMounted] = useState(false)
-
-	logs.debug("App component mounting", "App", { options })
-
-	useEffect(() => {
-		setMounted(true)
-		return () => {
-			setMounted(false)
-			logs.debug("App component unmounting", "App")
-		}
-	}, [])
-
-	if (!mounted) {
-		return null
-	}
-
 	return (
 		<JotaiProvider store={store}>
-			<Loader options={options} onExit={onExit} />
+			<UI options={options} onExit={onExit} />
 		</JotaiProvider>
 	)
 }
