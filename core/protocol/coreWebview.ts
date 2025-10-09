eb4da7e9e635be068677c71cd0f856c8bf8433f0
Eamon Nerbonne <eamon@nerbonne.org>
2025-10-09 22:51:17 +0200

    Remove protocol definitions (not used by autocomplete)
    continuedev commit: 80e550db472218b1e3bcea5f5020484c31063b3c

--- core/protocol/coreWebview.ts
+++
@@ -1,8 +0,0 @@
-import { ToCoreFromIdeOrWebviewProtocol } from "./core.js";
-import { ToWebviewFromIdeOrCoreProtocol } from "./webview.js";
-
-export type ToCoreFromWebviewProtocol = ToCoreFromIdeOrWebviewProtocol & {
-  didChangeSelectedProfile: [{ id: string }, void];
-  didChangeSelectedOrg: [{ id: string; profileId?: string }, void];
-};
-export type ToWebviewFromCoreProtocol = ToWebviewFromIdeOrCoreProtocol;
